bc79c22abac4a17e34a2127c31e4860cee7a1087
Frederick Kankam <fkwekukankam@gmail.com>
2025-08-29 20:45:50 +0000

    Showcase section

---
+++ src/sections/ShowcaseSection.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import { useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(ScrollTrigger);
+
+const ShowcaseSection = () => {
+    const sectionRef = useRef(null);
+    const project1Ref = useRef(null);
+    const project2Ref = useRef(null);
+    const project3Ref = useRef(null);
+
+
+    useGSAP(() => {
+        gsap.fromTo(sectionRef.current, 
+            {opacity: 0},
+            {opacity: 1, duration: 1.5},
+        )
+        
+        const project = [project1Ref.current, project2Ref.current, project3Ref.current];
+
+        project.forEach((card, index) => {
+        gsap.fromTo(
+            card,
+            {
+                y:50,
+                opacity:0
+                
+            },
+            {
+                y:0, 
+                opacity:1,
+                duration:1, 
+                delay:0.3 * (index + 1),
+                ScrollTrigger: {
+                    trigger: card,
+                    start: 'top bottom-=100'
+                }
+            }
+        )
+    });
+
+    })
+  return (
+    <section id="work" ref={sectionRef} className="app-showcase">
+        <div className="w-full">
+            <div className="showcaseLayout flex flex-col md:flex-row md:gap-8">
+                {/* LEFT */}
+                <div className="first-project-wrapper md:w-1/2" ref={project1Ref}>
+                    <div className= "image-wrapper">
+                        <img src="/images/project1.png" alt="Ryde" />
+                    </div>
+                    <div className="text-content">
+                        <h2>
+                            On-Demand Rides Made Simple with a PowerFull, User-Friendly App called Ryde
+                        </h2>
+                        <p className="text-white-50 md:text-xl">
+                            An app built with React Native, Expo, & TailwindCSS for a fast, user-friendly experience.
+                        </p>
+                    </div>
+                </div>
+                {/* RIGHT */}
+                <div className="project-list-wrapper md:w-1/2 flex flex-col md:gap-6 gap-4 overflow-hidden" ref={project2Ref}>
+                    <div className="project">
+                        <div className="image-wrapper bg-[#ffefdb]">
+                            <img src="/images/project2.png" alt="Library Management Platform" />
+                        </div>
+                        <h2>Library Management Platform</h2>
+                    </div>
+                    <div className="project" ref={project3Ref}>
+                        <div className="image-wrapper bg-[#ffe7eb]">
+                            <img src="/images/project3.png" alt="YC Directory" />
+                        </div>
+                        <h2>YC Directory- A startup Showcase</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  );
+}
+export default ShowcaseSection;
